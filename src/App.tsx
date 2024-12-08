89050e1fc26055133505674f440d04d718dcde4e
Yavuz KURU <yavuzkuru3@gmail.com>
2024-12-08 12:00:39 +0300

    [ react-router ] - add react router (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import loadable from '@loadable/component';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import {FC} from 'react';
+
+const Movies = loadable<FC>(
+  () => import('./pages/Movies/Movies')
+) as unknown as FC;
+const MovieDetail = loadable<FC>(
+  () => import('./pages/MoviesDetail/MoviesDetail')
+) as unknown as FC;
+
 function App() {
-  return <div>IMDB Finder</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/movies" replace={true} />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/movies/:id" element={<MovieDetail />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/components/TableGrid/TableGrid.tsx
@@ -0,0 +1,3 @@
+export function TableGrid() {
+  return <div>"TableGrid"</div>;
+}

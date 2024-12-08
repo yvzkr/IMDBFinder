25e2eb8c30e78c2fcecb3cbde41ab22a604b5a7f
yvzkr <yavuzkuru3@gmail.com>
2024-12-08 13:05:31 +0300

    [ feature-redux ] - Add redux

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import './styles/index.scss';
 import App from './App.tsx';
 import {BrowserRouter} from 'react-router-dom';
 import {SplashScreen} from './components';
+import {Provider} from 'react-redux';
+import {store} from './store';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Suspense fallback={<SplashScreen />}>
-        <App />
-      </Suspense>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Suspense fallback={<SplashScreen />}>
+          <App />
+        </Suspense>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
